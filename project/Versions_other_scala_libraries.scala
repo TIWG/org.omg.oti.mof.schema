ec45ca90c54d3d41ca5e1316771795cec294c5f7
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-07-27 17:26:14 +0000

    IMCEI-272 - Add connector to dump OTI MOF profiles/models for SPARK dependencies updates

--- project/Versions_other_scala_libraries.scala
+++ project/Versions_other_scala_libraries.scala
@@ -1,8 +1,8 @@
 object Versions_other_scala_libraries {
 
   // https://github.jpl.nasa.gov/imce/imce.third_party.other_scala_libraries
-  val version="2.7.0"
+  val version="2.11.0"
 
-  val scala_libraries_version = "2.7.0"
+  val scala_libraries_version = "2.11.0"
 
 }
